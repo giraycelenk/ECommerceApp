2357ba979f0127b674045cd8e5cd632f09b038b3
giraycelenk <a.giraycelenk@hotmail.com>
2024-09-25 22:21:43 +0300

    Pagination links added

--- ECommerceApp/ECommerceApp.Web/Controllers/HomeController.cs
+++ ECommerceApp/ECommerceApp.Web/Controllers/HomeController.cs
@@ -5,22 +5,31 @@ namespace ECommerceApp.Web.Controllers
 {
     public class HomeController:Controller
     {
+        public int pageSize = 3;
         private IECommerceRepository _eCommerceRepository;
         public HomeController(IECommerceRepository eCommerceRepository)
         {
             _eCommerceRepository = eCommerceRepository;
         }
-        public IActionResult Index()
+        public IActionResult Index(int page = 1)
         {
-            var products = _eCommerceRepository.Products.Select(p => new ProductViewModel{
-                Id= p.Id,
-                Name= p.Name,
-                Description= p.Description,
-                Price= p.Price,
-            }).ToList();
+            var products = _eCommerceRepository
+                .Products
+                .Skip((page-1)*pageSize)
+                .Select(p => 
+                    new ProductViewModel{
+                        Id= p.Id,
+                        Name= p.Name,
+                        Description= p.Description,
+                        Price= p.Price,
+                    }).Take(pageSize);
 
             return View(new ProductListViewModel {
-                Products = products
+                Products = products,
+                PageInfo = new PageInfo{
+                    ItemsPerPage = pageSize,
+                    TotalItems = _eCommerceRepository.Products.Count()
+                }
             });
         }
     }
--- ECommerceApp/ECommerceApp.Web/Models/ProductViewModel.cs
+++ ECommerceApp/ECommerceApp.Web/Models/ProductViewModel.cs
@@ -11,5 +11,12 @@ namespace ECommerceApp.Web.Models
     public class ProductListViewModel
     {
         public IEnumerable<ProductViewModel> Products { get; set; } = Enumerable.Empty<ProductViewModel>();
+        public PageInfo PageInfo { get; set; } = new();
+    }
+    public class PageInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int TotalPages => (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
     }
 }
